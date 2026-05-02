60d65517f7fe7bcfcec8e42ae247cd8b4ae9a7c9
shreyaa1804 <shreyayadav2222@gmail.com>
2026-05-02 23:45:33 +0530

    middleware auth

---
+++ backend/Middlewares/verifyAdmin.js
@@ -0,0 +1,8 @@
+const verifyAdmin = (req, res, next) => {
+  if (req.user.role !== "admin") {
+    return res.status(403).json({ msg: "Admin only" });
+  }
+  next();
+};
+
+module.exports = verifyAdmin;
