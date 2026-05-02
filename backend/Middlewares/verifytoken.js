0bd4813ab0aff7a580b464b67138491bd40334d1
shreyaa1804 <shreyayadav2222@gmail.com>
2026-05-02 23:49:13 +0530

    verfiy token

--- backend/Middlewares/verifytoken.js
+++ backend/Middlewares/verifytoken.js
@@ -1,21 +1,42 @@
-require('dotenv').config();
-const jwt = require('jsonwebtoken');
+// require('dotenv').config();
+// const jwt = require('jsonwebtoken');
 
-const verifyToken = (req, res , next)=>{
+// const verifyToken = (req, res , next)=>{
     
-       const token = req.headers['x-auth-token'];
-       jwt.verify(
-        token, process.env.JWT_SECRET,
+//        const token = req.headers['x-auth-token'];
+//        jwt.verify(
+//         token, process.env.JWT_SECRET,
     
-       (err,payload)=> {
-        if(err){
-            console.log(err);
-            res.status(500).json(err);
-        } else {
-            req.user = payload;
-            next();
-        }
-       })
-}
+//        (err,payload)=> {
+//         if(err){
+//             console.log(err);
+//             res.status(500).json(err);
+//         } else {
+//             req.user = payload;
+//             next();
+//         }
+//        })
+// }
+
+// module.exports = verifyToken;
+const jwt = require("jsonwebtoken");
+require('dotenv').config();
+const verifyToken = (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({ message: "No token provided" });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = decoded;
+    next();
+
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
 
 module.exports = verifyToken;
